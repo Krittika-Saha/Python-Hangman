561fcb522f4554d386b48a8b2e960575195f40ec
Krittika-Saha <63198376+Krittika-Saha@users.noreply.github.com>
2020-08-08 22:20:47 +0530

    Stage 2

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
 print("H A N G M A N")
 print("The game will be available soon.")
 
-# Hi
+
+Guess_word = input("Guess the word: ")
+word = "python"
+if Guess_word != word:
+    print("You are hanged!")
+else:
+    print("You survived!")
 
 
