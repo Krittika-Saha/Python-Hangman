96deaee7b986d3f67dc90ffec6825fb21dfad491
Krittika-Saha <63198376+Krittika-Saha@users.noreply.github.com>
2020-08-09 13:47:04 +0530

    Added hints
    Added some hints for help.

--- index.py
+++ index.py
@@ -2,9 +2,12 @@ import random
 
 print("H A N G M A N")
 print("The game will be available soon.")
-Guess_word = input("Guess the word: ")
 Word_List = ["python", "kotlin", 'java', "javascript"]
 Random_Word = random.choice(Word_List)
+shown = Random_Word[0:3]
+hidden = "-" * (len(Random_Word) - 3)
+word = shown + hidden
+Guess_word = input("Guess the word: "+ word)
 if Random_Word != Guess_word:
     print("You are hanged!")
 else:
