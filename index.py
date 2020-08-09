692e10512d828d73da3e45616824f8f5ec650b7f
Krittika-Saha <63198376+Krittika-Saha@users.noreply.github.com>
2020-08-09 10:23:59 +0530

    Guess list added

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
+import random
+
 print("H A N G M A N")
 print("The game will be available soon.")
-
-
 Guess_word = input("Guess the word: ")
-word = "python"
-if Guess_word != word:
+Word_List = ["python", "kotlin", 'java', "javascript"]
+Random_Word = random.choice(Word_List)
+if Random_Word != Guess_word:
     print("You are hanged!")
 else:
     print("You survived!")
-
-
