af48ac0ae535b039001f178aca92e920edaea991
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-11-28 17:01:40 +0530

    added some modifications to challenge 1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
-user_choice = input("Guess a letter: ")
+user_choice = input("Guess a letter: ").lower()
 
 for i in chosen_word:
   if i == user_choice:
