d992dfb70fcce9a35b581223eeed6e77f2a27506
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-11-28 19:28:44 +0530

    Stage 3 completed

--- main.py
+++ main.py
@@ -10,26 +10,30 @@ print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
-user_choice = input("Guess a letter: ").lower()
 
 for i in chosen_word:
   display.append('_')
 
 
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+while '_' in display:
+  user_choice = input("Guess a letter: ").lower()
 
-for index in range(len(chosen_word)):
-  letter = chosen_word[index]
-  if letter == user_choice:
-    display[index] = letter
 
-print(display)
+  #TODO-2: - Loop through each position in the chosen_word;
+  #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+  #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+  for index in range(len(chosen_word)):
+    letter = chosen_word[index]
+    if letter == user_choice:
+      display[index] = letter
+
+  print(display)
+
+print('You Won!')
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
