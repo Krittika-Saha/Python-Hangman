86417c55bb6d28cbdc0449bec4f6ee5e0b2594b6
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-11-28 20:34:59 +0530

    Stage 4 finished

--- main.py
+++ main.py
@@ -1,10 +1,64 @@
-    #Step 2
 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
-#Testing code
+lives = 6
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 print(f'Pssst, the solution is {chosen_word}.')
 
 
@@ -15,25 +69,24 @@ for i in chosen_word:
   display.append('_')
 
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
 while '_' in display:
   user_choice = input("Guess a letter: ").lower()
 
-
-  #TODO-2: - Loop through each position in the chosen_word;
-  #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-  #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
   for index in range(len(chosen_word)):
     letter = chosen_word[index]
-    if letter == user_choice:
+    if user_choice == letter:
       display[index] = letter
 
-  print(display)
+  if user_choice not in chosen_word:
+    lives -= 1
+    print(stages[lives])
+    if lives == 0:
+      break  
 
-print('You Won!')
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+  print(f"{' '.join(display)}")
+
+if lives == 0:
+  print('You Lost')
+else:
+  print('You Won!')
